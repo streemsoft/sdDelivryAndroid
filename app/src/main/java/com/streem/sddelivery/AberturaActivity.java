998138bb8457e6b024199c79e00936bfb5fa0e70
Josué Azevedo <39640061+streemsoft@users.noreply.github.com>
2018-06-12 01:00:44 -0300

    c

---
+++ app/src/main/java/com/streem/sddelivery/AberturaActivity.java
@@ -0,0 +1,13 @@
+package com.streem.sddelivery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AberturaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_abertura);
+    }
+}
